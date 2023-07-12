614224d667c3fc5283691d8a9e45780efde4151d
ayamba-coder <takuntohmartin@gmail.com>
2023-07-12 20:02:15 +0100

    60% workdone

---
+++ Agroplus/README.md
@@ -0,0 +1,6 @@
+# AgroPLus
+My first personal portfolio project with no layout systems like grid nor flexbox.<br>
+A non-responsive Agricultural Food supply chain company.
+<br>
+Please View on breakpoints above ||= 1024px 
+:)
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var nav = document.querySelector('nav');
 var img = document.querySelector('.banner');
-var description = document.querySelector('.description');
-var bannerHeight = description.offsetTop - nav.offsetHeight
+var card_container = document.querySelector('.card_container');
+var bannerHeight = card_container.offsetTop - nav.offsetHeight
+var bannerHeading = document.querySelector(".banner h1");
+var personality = ['a web Developer','a Teacher','a Trainer']
 
 window.addEventListener("scroll",function(){
     if (window.scrollY > bannerHeight) {
@@ -9,4 +11,20 @@ window.addEventListener("scroll",function(){
     }else{
         nav.classList.remove('theme')
     }
-})
+});
+
+var charCounter = 0;
+var personalityCounter = 0
+
+function animateWords(){
+    bannerHeading.innerText = `I am ${personality[personalityCounter].slice(0,charCounter+1)}`;
+    charCounter++;
+    if (charCounter === personality[personalityCounter].length) {
+        personalityCounter++;
+        charCounter = 0;
+    }
+    if(personalityCounter === personality.length){
+        personalityCounter=0
+    }
+}
+setInterval(animateWords, 400);
